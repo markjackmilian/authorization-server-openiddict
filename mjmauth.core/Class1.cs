e61ab362740d34e6ede0d1fdd3493a2afe209e6c
Marco <emme.milani@hotmail.it>
2021-11-28 17:18:48 +0100

    add dependencies

--- mjmauth.core/Class1.cs
+++ mjmauth.core/Class1.cs
@@ -1,7 +1,18 @@
-﻿
-namespace mjmauth.core
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace mjmauth.core;
+
+public static class Class1
 {
-    public class Class1
+    public static void addgino(this IServiceCollection services)
     {
+        services.AddControllersWithViews();
+            
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.LoginPath = "/account/login";
+            });
     }
 }
