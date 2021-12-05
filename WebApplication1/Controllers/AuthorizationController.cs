ebf3bc33553e6cd723ed945e42f15835b35a2fd9
Marco <emme.milani@hotmail.it>
2021-12-06 00:13:39 +0100

    Add OpenidDict on identity
    setup clientcredentials and authorizationcode

---
+++ WebApplication1/Controllers/AuthorizationController.cs
@@ -0,0 +1,153 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        [HttpGet("~/connect/authorize")]
+        [HttpPost("~/connect/authorize")]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> Authorize()
+        {
+            var request = this.HttpContext.GetOpenIddictServerRequest() ??
+                          throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+            
+            // Retrieve the user principal stored in the authentication cookie.
+            var result = await this.HttpContext.AuthenticateAsync(IdentityConstants.ApplicationScheme);
+
+            // If the user principal can't be extracted, redirect the user to the login page.
+            if (!result.Succeeded)
+            {
+                return this.Challenge(
+                    authenticationSchemes: IdentityConstants.ApplicationScheme,
+                    properties: new AuthenticationProperties
+                    {
+                        RedirectUri = this.Request.PathBase + this.Request.Path + QueryString.Create(
+                            this.Request.HasFormContentType ? this.Request.Form.ToList() : this.Request.Query.ToList())
+                    });
+            }
+
+            // Create a new claims principal
+            var claims = new List<Claim>
+            {
+                // 'subject' claim which is required
+                new Claim(OpenIddictConstants.Claims.Subject, result.Principal.Identity.Name),
+                new Claim("some claim", "some value").SetDestinations(OpenIddictConstants.Destinations.AccessToken),
+                new Claim(OpenIddictConstants.Claims.Email, "some@email").SetDestinations(OpenIddictConstants.Destinations.IdentityToken)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            // Set requested scopes (this is not done automatically)
+            claimsPrincipal.SetScopes(request.GetScopes());
+
+            // Signing in with the OpenIddict authentiction scheme trigger OpenIddict to issue a code (which can be exchanged for an access token)
+            return this.SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+        
+        [HttpPost("~/connect/token")]
+        public async Task<IActionResult> Exchange()
+        {
+            var request = this.HttpContext.GetOpenIddictServerRequest() ??
+                          throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+            ClaimsPrincipal claimsPrincipal;
+
+            if (request.IsClientCredentialsGrantType())
+            {
+                // Note: the client credentials are automatically validated by OpenIddict:
+                // if client_id or client_secret are invalid, this action won't be invoked.
+
+                var identity = new ClaimsIdentity(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+
+                // Subject (sub) is a required field, we use the client id as the subject identifier here.
+                identity.AddClaim(OpenIddictConstants.Claims.Subject, request.ClientId ?? throw new InvalidOperationException());
+
+                // Add some claim, don't forget to add destination otherwise it won't be added to the access token.
+                identity.AddClaim("some-claim", "some-value", OpenIddictConstants.Destinations.AccessToken);
+
+                claimsPrincipal = new ClaimsPrincipal(identity);
+
+                claimsPrincipal.SetScopes(request.GetScopes());
+            }
+
+            else if (request.IsAuthorizationCodeGrantType())
+            {
+                // Retrieve the claims principal stored in the authorization code
+                claimsPrincipal = (await this.HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+            }
+            
+            else if (request.IsRefreshTokenGrantType())
+            {
+                // Retrieve the claims principal stored in the refresh token.
+                claimsPrincipal = (await this.HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+            }
+            else if (request.IsPasswordGrantType())
+            {
+                // Validate the user credentials.
+                // Note: to mitigate brute force attacks, you SHOULD strongly consider
+                // applying a key derivation function like PBKDF2 to slow down
+                // the password validation process. You SHOULD also consider
+                // using a time-constant comparer to prevent timing attacks.
+                // if (request.Username != "alice@wonderland.com" ||
+                //     request.Password != "P@ssw0rd")
+                // {
+                //     return Forbid(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+                // }
+                
+                // Create a new ClaimsIdentity holding the user identity.
+                var identity = new ClaimsIdentity(
+                    OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
+                    OpenIddictConstants.Claims.Name,
+                    OpenIddictConstants.Claims.Role);
+                
+                // Add a "sub" claim containing the user identifier, and attach
+                // the "access_token" destination to allow OpenIddict to store it
+                // in the access token, so it can be retrieved from your controllers.
+                identity.AddClaim(OpenIddictConstants.Claims.Subject,
+                    "71346D62-9BA5-4B6D-9ECA-755574D628D8",
+                    OpenIddictConstants.Destinations.AccessToken);
+                identity.AddClaim(OpenIddictConstants.Claims.Name, request.Username,
+                    OpenIddictConstants.Destinations.AccessToken);
+                // ... add other claims, if necessary.
+                
+                claimsPrincipal = new ClaimsPrincipal(identity);
+
+                claimsPrincipal.SetScopes(request.GetScopes());
+                // Ask OpenIddict to generate a new token and return an OAuth2 token response.
+            }
+
+            else
+            {
+                throw new InvalidOperationException("The specified grant type is not supported.");
+            }
+
+            // Returning a SignInResult will ask OpenIddict to issue the appropriate access/identity tokens.
+            return this.SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+        
+        [Authorize(AuthenticationSchemes = OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)]
+        [HttpGet("~/connect/userinfo")]
+        public async Task<IActionResult> Userinfo()
+        {
+            var claimsPrincipal = (await this.HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)).Principal;
+
+            return this.Ok(new
+            {
+                Name = claimsPrincipal.GetClaim(OpenIddictConstants.Claims.Subject),
+                Occupation = "Developer",
+                Age = 43
+            });
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using WebApplication1;
 using WebApplication1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +8,64 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    {
+        options.UseSqlServer(connectionString);
+        // Register the entity sets needed by OpenIddict.
+        options.UseOpenIddict();
+    }
+);
+
+builder.Services.AddOpenIddict()
+
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the EF Core stores/models.
+        options.UseEntityFrameworkCore()
+            .UseDbContext<ApplicationDbContext>();
+    })
+
+    // Register the OpenIddict server components.
+    .AddServer(options =>
+    {
+        options
+            .AllowClientCredentialsFlow()
+            .AllowAuthorizationCodeFlow()
+            .RequireProofKeyForCodeExchange()
+            .AllowPasswordFlow()
+            // .AcceptAnonymousClients()
+            .AllowRefreshTokenFlow();
+
+        options
+            .SetTokenEndpointUris("/connect/token")
+            .SetAuthorizationEndpointUris("/connect/authorize")
+            .SetUserinfoEndpointUris("/connect/userinfo");
+
+        // Encryption and signing of tokens
+        options
+            .AddEphemeralEncryptionKey()
+            .AddEphemeralSigningKey()
+            .DisableAccessTokenEncryption();
+
+        // Register scopes (permissions)
+        options.RegisterScopes("api");
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options
+            .UseAspNetCore()
+            .EnableTokenEndpointPassthrough()
+            .EnableAuthorizationEndpointPassthrough()
+            .EnableUserinfoEndpointPassthrough();            
+    });
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHostedService<TestData>();
+
 
 var app = builder.Build();
 
